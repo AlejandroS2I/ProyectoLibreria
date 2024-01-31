423d295025a41e5fa3dd046ea85f449ba88bcfec
hugo <desplieguedaw23@hotmail.com>
2024-01-31 21:10:13 +0100

    delete hecho

---
+++ php/deleteTarea.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_GET['id'])){
+    
+    require_once 'connect.php';
+    $db = 'tareas';
+
+    $conexion = conectar($db);
+    $parametros = array(":id"=>$_GET['id']);
+    $sql = "DELETE FROM ttareas WHERE id = :id";
+    $pdo = $conexion->prepare($sql);
+    $pdo->execute($parametros);     
+}
+?>
