888b2b90b26bb7a23e6cc97aecd9b114af5f08b1
hugo <desplieguedaw23@hotmail.com>
2024-02-01 16:59:57 +0100

    arreglo del delete, falta el insert

--- php/deleteTarea.php
+++ php/deleteTarea.php
@@ -1,10 +1,10 @@
 <?php
-if (isset($_GET['id'])) {
+if (isset($_POST['id'])) {
     require_once 'connect.php';
     $db = 'tareas';
 
     $conexion = conectar($db);
-    $parametros = array(":id" => $_GET['id']);
+    $parametros = array(":id" => $_POST['id']);
     $sql = "DELETE FROM ttareas WHERE ID = :id";
     $pdo = $conexion->prepare($sql);
     $pdo->execute($parametros);
