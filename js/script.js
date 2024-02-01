74c0a5cd0047d2671e05a6b61184b3ea3c942a8b
hugo <desplieguedaw23@hotmail.com>
2024-02-01 17:58:41 +0100

    js hecho la parte de act y mostr sin mostrar()

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
+mostrarTareas();
 //insertar
 function insertTarea(titulo, descripcion, hecha) {
 
 }
 //actualizar
-
+function actualizarTareas(id, titulo, descripcion, hecha, mostrar){
+    $.ajax({
+        type: 'POST',
+        url: 'php/updateTarea',
+        data:{
+            id:id,
+            titulo:titulo,
+            descripcio: descripcion,
+            hecha: hecha,
+        }
+        ,
+        dataType: 'json',
+        success: mostrar,
+        error: function (error) {
+            console.log("Error al cambiar la lista de tareas: " + error);
+        }
+    })
+}
 //eliminar
 
 //seleccionar
-
+function mostrarTareas(mostrar) {
+        $.ajax({
+        type: 'GET',
+        url: 'php/selectTarea.php',
+        dataType: 'json',
+        success: mostrar,
+        error: function (error) {
+            console.log("Error al obtener la lista de tareas: " + error);
+        }
+    });
+}
